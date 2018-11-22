07c2c9f48b86d3a69acf2459ef7c680bd2eebec4
unknown <btstar1998@gmail.com>
2018-11-23 02:59:20 +0900

    DB directory setting and mkdir

--- src/main/java/DatabaseHandler.java
+++ src/main/java/DatabaseHandler.java
@@ -1,2 +1,20 @@
+import java.io.File;
+import java.nio.file.*;
+import java.sql.*;
+
 public class DatabaseHandler {
+    private Connection connection = null;
+    private Statement statement = null;
+
+    public DatabaseHandler() {
+        Path dbFolder;
+
+        if (System.getProperty("os.name").contains("Windows")) {
+            dbFolder = Paths.get(System.getenv("APPDATA"), "delivery_monitor");
+        } else {
+            dbFolder = Paths.get("/usr/share", "delivery_monitor");
+        }
+
+        new File(dbFolder.toString()).mkdirs();
+    }
 }
