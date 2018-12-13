1abae9e0c7cc55a50a87caa669ed64a7f117669f
unknown <btstar1998@gmail.com>
2018-12-13 17:10:04 +0900

    Fixed non-windows DB path

--- src/main/java/cose102/delivery_monitor/db_handler/DatabaseHandler.java
+++ src/main/java/cose102/delivery_monitor/db_handler/DatabaseHandler.java
@@ -29,7 +29,7 @@ public class DatabaseHandler {
         if (System.getProperty("os.name").contains("Windows")) {
             dbFolder = Paths.get(System.getenv("APPDATA"), "delivery_monitor");
         } else {
-            dbFolder = Paths.get("/usr/share", "delivery_monitor");
+            dbFolder = Paths.get("~/usr/share", "delivery_monitor");
         }
 
         new File(dbFolder.toString()).mkdirs();
